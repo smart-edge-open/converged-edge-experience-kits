d404ccfc3b4c2deaefb1bfc9ee5a4a5c88029d2a
Patryk Matuszak <47594217+patrykxmatuszak@users.noreply.github.com>
2020-04-21 14:43:32 +0200

    Update deploy_ne.sh with single-node cluster (#293)

--- deploy_ne.sh
+++ deploy_ne.sh
@@ -3,10 +3,31 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2019-2020 Intel Corporation
 
+# Usage:
+#  Regular Network Edge mode:
+#   ./deploy_ne.sh               deploy both controller & nodes
+#   ./deploy_ne.sh c[ontroller]  deploy only controller
+#   ./deploy_ne.sh n[odes]       deploy only nodes
+#
+#  Single-node cluster:
+#   ./deploy_ne.sh s[ingle]      deploy single-node cluster playbook
+
+set -eu
+
 source scripts/ansible-precheck.sh
 source scripts/task_log_file.sh
 source scripts/parse_args.sh
 
+filter=${1:-}
+limit=""
+
+if [[ "${filter}" == s* ]]; then
+    playbook="single_node_network_edge.yml"
+else
+    playbook="network_edge.yml"
+    limit=$(get_limit ${filter})
+fi
+
 ansible-playbook -vv \
-    ./network_edge.yml \
-    --inventory inventory.ini ${limit_param}
+    "${playbook}" \
+    --inventory inventory.ini ${limit}
