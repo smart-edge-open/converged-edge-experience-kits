73aedc60e4e55271843f86faf87555bc8c4c0016
PatrykDiak <47568946+PatrykDiak@users.noreply.github.com>
2020-06-05 10:14:55 +0200

    Minimal flavor (#381)
    Introduce flavor mechanism with minimal flavor example

--- deploy_ne.sh
+++ deploy_ne.sh
@@ -5,12 +5,12 @@
 
 # Usage:
 #  Regular Network Edge mode:
-#   ./deploy_ne.sh               deploy both controller & nodes
-#   ./deploy_ne.sh c[ontroller]  deploy only controller
-#   ./deploy_ne.sh n[odes]       deploy only nodes
+#   ./deploy_ne.sh [-f <flavor>]               deploy both controller & nodes
+#   ./deploy_ne.sh [-f <flavor>] c[ontroller]  deploy only controller
+#   ./deploy_ne.sh [-f <flavor>] n[odes]       deploy only nodes
 #
 #  Single-node cluster:
-#   ./deploy_ne.sh s[ingle]      deploy single-node cluster playbook
+#   ./deploy_ne.sh [-f <flavor>] s[ingle]      deploy single-node cluster playbook
 
 set -eu
 
@@ -18,8 +18,45 @@ source scripts/ansible-precheck.sh
 source scripts/task_log_file.sh
 source scripts/parse_args.sh
 
-filter=${1:-}
+flavor=""
+while getopts ":f:" o; do
+    case "${o}" in
+        f)
+            flavor=${OPTARG}
+            ;;
+        *)
+            echo "Invalid flag"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "${flavor}" ]]; then
+    echo "No flavor provided"
+    echo -e "   $0 [-f <flavor>] <filter>. Available flavors: $(ls -m flavors)"
+else
+    flavor_path="${PWD}/flavors/${flavor}"
+    if [[ ! -d "${flavor_path}" ]]; then
+        echo "Flavor ${1} does not exist[${flavor_path}]"
+        exit 1
+    fi
+
+    for f in "${flavor_path}"/*.yml
+    do
+        fname=$(basename "${f}" .yml)
+        dir="${PWD}/group_vars/${fname}"
+        if [[ ! -d "${dir}" ]]; then
+            echo "${f} does not match a directory in group_vars:"
+            ls "${PWD}/group_vars/"
+            exit 1
+        fi
+        ln -sfn "${f}" "${dir}/30_${flavor}_flavor.yml"
+    done
+fi
+
 limit=""
+filter="${1:-}"
 
 if [[ "${filter}" == s* ]]; then
     playbook="single_node_network_edge.yml"
