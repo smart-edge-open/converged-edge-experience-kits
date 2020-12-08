853c5d6f2091ee2038191c2fe0a841c0b31ec0c0
token <68195420+jiangzhg@users.noreply.github.com>
2020-12-08 20:17:23 +0800

    Jzg debug (#671)
    * remove hardcode paths
    
    * revert code for normal mode
    
    * find cni directory for offline mode
    
    * precheck both in oek and x-oek under offline mode
    
    Co-authored-by: tongtongxiaopeng1979 <46148613+tongtongxiaopeng1979@users.noreply.github.com>

--- scripts/ansible-precheck.sh
+++ scripts/ansible-precheck.sh
@@ -16,18 +16,20 @@ fi
 
 # Check the value of offline_enable
 TOP_PATH=$(cd "$(dirname "$0")";pwd)
-if grep "offline_enable" "${TOP_PATH}/group_vars/all/10-default.yml" | grep -qE "[T|t]rue"; then
-  if [ ! -e "${TOP_PATH}/roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz" ]; then
-    echo "ERROR: Miss package: roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz!"
+if grep "offline_enable" "$TOP_PATH"/group_vars/all/*.yml | grep -qE "[T|t]rue"; then
+  prepackagePath=""
+  if [ -e "${TOP_PATH}/roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz" ]; then
+     prepackagePath="${TOP_PATH}/roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz"
+  elif [ -e "${TOP_PATH}/oek/roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz" ]; then
+     prepackagePath="${TOP_PATH}/oek/roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz"
+  else
+    echo "ERROR: Miss package: [oek/]roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz!"
     exit 1
   fi
   tmpDir=$(mktemp -d)
-  cp "${TOP_PATH}/roles/offline_roles/unpack_offline_package/files/prepackages.tar.gz" "${tmpDir}"
-
-  cd "${tmpDir}" && tar xzvf "${tmpDir}/prepackages.tar.gz"
-  yum localinstall -y "${tmpDir}"/*
-  rm -rf "${tmpDir}"
-  cd "${TOP_PATH}"
+  tar xvf "$prepackagePath" -C "$tmpDir"
+  yum localinstall -y "$tmpDir"/*
+  rm -rf "$tmpDir"
 fi
 
 if ! command -v ansible-playbook 1>/dev/null; then
